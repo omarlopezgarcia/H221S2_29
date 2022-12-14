03cd914c8cbc07bf3b1dc7e332c20f2807368000
Omar López García <111783739+omarlopezgarcia@users.noreply.github.com>
2022-12-14 12:29:59 -0500

    cambios nuevos

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('index.html'))
+app.get('/', (req, res) => {res.sendFile('/home/ubuntu/project/H221S2_29/index.html')});
 
 app.listen(3000);
 console.log('Server on port 3000')
