a1a99dc6f4513fa4c768fc822bc33ef7418b3c29
Omar López García <111783739+omarlopezgarcia@users.noreply.github.com>
2022-12-17 20:43:53 -0500

    cambios

--- resources/registro.js
+++ resources/registro.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:3000/api/MATRICULA/";
+const url = "http://18.234.195.219:3000/api/MATRICULA/";
 let resultados = '';
 const formArticulo = document.querySelector("form");
 const NOMMAT = document.getElementById("NOMMAT");
